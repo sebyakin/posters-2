93efa929909dcfe7490bdc26491e5f6ecb1fdd25
sebyakin <46461635+sebyakin@users.noreply.github.com>
2020-04-04 16:45:03 +0300

    new-commit

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-let tl = document.querySelector('.t-l');
-let ar = document.querySelector('.ar');
+window.addEventListener('load', () => {
 
-let info = document.querySelector('.float');
-let popup = document.querySelector('.popup');
+    let tip = document.querySelector('.tip');
 
-info.addEventListener('click', function() {
-    popup.classList.toggle('show');
-    // info.textContent = ('×');
+    document.querySelector(".preloader").style.visibility = "hidden";
+
+    let timerShow = setTimeout(() => showTip(), 1000);
+    let timerHide = setTimeout(() => hideTip(), 5000);
+    // setTimeout(() => {
+    //   clearInterval(timerId);
+    // }, 2500);
+
+
+    function showTip () {
+        
+        tip.classList.add('tip-show');
+    }
+
+    function hideTip () {
+        tip.classList.remove('tip-show');
+    }
+
+
+
+    let tl = document.querySelector('.t-l');
+    let ar = document.querySelector('.ar');
+
+    let info = document.querySelector('.info');
+    let popup = document.querySelector('.popup');
+
+    info.addEventListener('click', function() {
+        popup.classList.toggle('show');
+        // info.textContent = ('×');
+    });
+
 });
 
 
 
-
-
-
-
-// tl.addEventListener('mouseenter', function() {
-//     ar.classList.add('show');
-// });
-
-// tl.addEventListener('mouseleave', function() {
-//     ar.classList.remove('show');
-// });
