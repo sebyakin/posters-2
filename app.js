11de2af38611a919d0bc26dd027d62191931a832
sebyakin <46461635+sebyakin@users.noreply.github.com>
2020-04-04 14:10:58 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let tl = document.querySelector('.t-l');
+let ar = document.querySelector('.ar');
+
+let info = document.querySelector('.float');
+let popup = document.querySelector('.popup');
+
+info.addEventListener('click', function() {
+    popup.classList.toggle('show');
+    // info.textContent = ('×');
+});
+
+
+
+
+
+
+
+// tl.addEventListener('mouseenter', function() {
+//     ar.classList.add('show');
+// });
+
+// tl.addEventListener('mouseleave', function() {
+//     ar.classList.remove('show');
+// });
