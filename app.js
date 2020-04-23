b6b2d706baaf782cea54c82d4987eece0159ffe5
sebyakin <46461635+sebyakin@users.noreply.github.com>
2020-04-23 09:59:41 +0300

    z

--- app.js
+++ app.js
@@ -1,36 +1,45 @@
 window.addEventListener('load', () => {
 
     let tip = document.querySelector('.tip');
+    let warning = document.querySelectorAll('.warning');
+
+    console.log(warning);
 
     document.querySelector(".preloader").style.visibility = "hidden";
 
-    let timerShow = setTimeout(() => showTip(), 1000);
-    let timerHide = setTimeout(() => hideTip(), 5000);
-    // setTimeout(() => {
-    //   clearInterval(timerId);
-    // }, 2500);
 
+    // function show () {
+    //     let num = Math.floor(Math.random() * 4); 
+    //     warning[num].classList.add('warning-shown');
+    //     setTimeout(() => warning[num].classList.remove('warning-shown'), 2000);
+    // }
 
-    function showTip () {
-        
-        tip.classList.add('tip-show');
-    }
+    // let timerShow = setInterval(() => show(), 7000);
 
-    function hideTip () {
-        tip.classList.remove('tip-show');
-    }
+    // setTimeout(() => { clearInterval(timerShow);}, 20000);
 
 
 
-    let tl = document.querySelector('.t-l');
-    let ar = document.querySelector('.ar');
+
+    // let timerShow = setTimeout(() => showTip(), 1000);
+    // let timerHide = setTimeout(() => hideTip(), 5000);
+
+
+    // function showTip () {
+    //     tip.classList.add('tip-show');
+    // }
+
+    // function hideTip () {
+    //     tip.classList.remove('tip-show');
+    // }
+
+
 
     let info = document.querySelector('.info');
     let popup = document.querySelector('.popup');
 
     info.addEventListener('click', function() {
         popup.classList.toggle('show');
-        // info.textContent = ('×');
     });
 
 });
